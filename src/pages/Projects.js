13f39da7fca78c87ab91a89d7c68c5b287c7978f
vinicius <vi_guepardo@hotmail.com>
2022-08-22 16:12:03 -0300

    initialized projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,13 +13,18 @@ export function Projects() {
                 <p className="my-[20px] text-[1.1em]">Visitando meu Github você verá as menções para cada projeto e de onde eles vieram.</p>
             </div>
 
-            <div>
+            <div className="grid grid-cols-3 gap-4 w-[100rem] h-[100rem]"
+>
                 {isLoading && <p>Carregando...</p>}
-                <ul>
-                    {repositories?.map(repo => (
-                        <li key={repo.id}>{repo.name}</li>
-                    ))}
-                </ul>
+
+                {repositories?.map(repo => (
+                    <div key={repo.id} className="flex justify-center items-center text-[1.8rem] font-bold bg-black text-white hover:bg-[#fec63e] hover:text-[#333] transition-colors">
+                        <a href={repo.html_url} alt="Repositório" target="_blank" rel="noreferrer">
+                            {repo.name}
+                        </a>
+                    </div>
+                ))}
+
             </div>
         </div>
     )
