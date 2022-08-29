5fac073e691c4d69e7bcfb77125570142ea02222
vinicius <vi_guepardo@hotmail.com>
2022-08-29 12:58:24 -0300

    add mobile first on about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,14 @@
 export function About() {
     return (
-        <div className="flex flex-col w-[100%] min-h-[100vh] p-[100px] bg-black" id="about">
-            <div className="text-center text-[#fff]" data-aos="fade-down" data-aos-duration="1000">
+        <div className="flex flex-col w-[100%] min-h-[100vh] lg:p-[100px] bg-black" id="about">
+            <div className="text-center text-[#fff] mt-4 lg:mt-0" data-aos="fade-down" data-aos-duration="1000">
                 <h2 className="relative text-[1.8em] tracking-[1px] font-bold before:absolute before:content-[''] before:w-[60px] before:h-[4px] before:left-[50%] before:bottom-[-10px] before:translate-x-[-50%] before:bg-[#fec63e]">Sobre mim</h2>
                 <p className="my-[20px] text-[1.1em]">Programador Front-End de São Paulo.</p>
             </div>
 
-            <div className="flex justify-between mt-[40px]">
-                <div className="text-center text-[#fff] min-w-[49%] w-[49%]" data-aos="fade-right" data-aos-duration="2000">
-                    <p className="my-[20px] text-[1.1em] text-left">
+            <div className="flex flex-col justify-center items-center p-[30px] lg:flex-row lg:justify-between lg:mt-[40px] lg:p-[0] lg:items-start">
+                <div className="text-center text-[#fff] lg:min-w-[49%] lg:w-[49%]" data-aos="fade-right" data-aos-duration="2000">
+                    <p className="mb-[20px] lg:mt-[20px] text-[1.1em] text-left">
                         Tenho 23 anos e sou formado no Colégio Objetivo de Guarujá, aonde morei a minha vida inteira até metade de 2020, foi quando me mudei para São Paulo com meus pais em busca de melhores oportunidades de vida. <br /> <br />
                         As coisas que você encontrará aqui serão apenas um pouco de quem eu sou, mas espero que seja o suficiente para que instigue alguém a querer me conhecer melhor, me dar uma oportunidade e ver até aonde eu posso ir. <br /> <br />
                         Sempre fui fascinado por tecnologia e após me mudar tive a oportunidade de começar a estudar sobre isso, aonde realmente me vi tendo um futuro de verdade e que fosse próspero o bastante para me sustentar e futuramente também uma família. <br /> <br />
@@ -16,7 +16,7 @@ export function About() {
                     </p>
                 </div>
 
-                <div className="ml-[40px]" data-aos="fade-left" data-aos-duration="2000">
+                <div className="lg:ml-[40px]" data-aos="fade-left" data-aos-duration="2000">
                     <img
                         src="https://muvias.github.io/Portfolio/images/deitada.jpeg"
                         alt="Minha foto"
